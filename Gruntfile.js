83ba059522ded12e8fa37e2dadbb4d54503b5869
Jiri Ludvar <jiri.ludvar@aaaauto.cz>
2015-05-22 14:56:17 +0200

    Přidání Legacy

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
                     ]
                 },
                 files: {
-                    "css/style.css": ["less/basic.less","less/tablet.less","less/pc.less"]
+                    "css/style.css": ["less/basic.less","less/tablet.less","less/pc.less","less/legacy.less"]
                 }
             }
         },
